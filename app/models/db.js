0968ba1f2b902dc3ac593fc5a45aa7644bd77198
AdelleMcateer <adellemaca@gmail.com>
2021-03-10 20:59:45 +0000

    Seeding the database. Adding candidates.

--- app/models/db.js
+++ app/models/db.js
@@ -22,3 +22,18 @@ db.on("disconnected", function() {
 db.once("open", function() {
   console.log(`database connected to ${this.name} on ${this.host}`);
 });
+
+async function seed() {
+  var seeder = require('mais-mongoose-seeder')(Mongoose);
+  const data = require('./seed-data.json');
+  const Donation = require('./donation');
+  const Candidate = require('./candidate.js');
+  const User = require('./user');
+  const dbData = await seeder.seed(data, { dropDatabase: false, dropCollections: true });
+  console.log(dbData);
+}
+
+db.once('open', function () {
+  console.log(`database connected to ${this.name} on ${this.host}`);
+  seed();
+});
